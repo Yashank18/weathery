3e8cfa6369712c56c958ed887206d09588466184
Yashank18 <yashank7696@gmail.com>
2021-02-03 20:46:36 +0530

    Unit Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import CardArea from './Container/CardArea/CardArea';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+it('Weather Cards Gives an Div Container',()=>{
+  const renderer = new ShallowRenderer();
+   renderer.render(<CardArea/>);
+   const Button= renderer.getRenderOutput();
+   expect(Button.type).toBe('div');
 });
